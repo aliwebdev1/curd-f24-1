efe87b03089914c1474c468fc2ab91325e361bbb
nayeem19999 <nhnayeem13@gmail.com>
2025-05-08 19:59:38 +0600

    url sequire

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ const port = 3000;
 // middleware
 app.use(cors());
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.guep9xh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-console.log(uri);
-
+const uri = process.env.url;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
