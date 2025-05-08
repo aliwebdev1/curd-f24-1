f825d3cf6439731ad9af807d5cf289b0e8d65cd6
nayeem19999 <nhnayeem13@gmail.com>
2025-05-08 19:48:44 +0600

    read

--- index.js
+++ index.js
@@ -5,8 +5,42 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// middleware
+// middleware    KRlF61SaQmApbi1b     basic_card
 app.use(cors());
+
+
+
+
+const uri = "mongodb+srv://basic_card:KRlF61SaQmApbi1b@cluster0.guep9xh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('Basic');
+        const userCollection = database.collection("Users");
+
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await userCollection.find(query).toArray();
+            res.send(users)
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 
 
